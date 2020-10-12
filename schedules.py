54ca8f980519afc4ce3a7d1e43d20646f1dd2e47
Christopher Liu <christopherliu@sorcero.com>
2020-10-12 14:15:43 -0400

    Add links to schedule

--- schedules.py
+++ schedules.py
@@ -4,17 +4,17 @@ def get_schedule(letter, periods):
             <tr>
                 <th>1</th>
                 <td>9:10 AM - 10:05 AM</td>
-                <td>AP US History</td>
+                <td><a href="https://classroom.google.com/u/0/c/MTU5ODcyMTk0NTQ3" target="_blank">AP US History</a></td>
             </tr>
             <tr>
                 <th>2</th>
                 <td>10:15 AM - 11:10 AM</td>
-                <td>Calc BC</td>
+                <td><a href="https://us02web.zoom.us/j/83208699689?pwd=SDJrUnBZK3JOU0dxSjREUnN0WkQ0QT09" target="_blank">Calc BC</a></td>
             </tr>
             <tr>
                 <th>3</th>
                 <td>11:20 AM - 12:15 PM</td>
-                <td>Calc BC</td>
+                <td><a href="https://us02web.zoom.us/j/83208699689?pwd=SDJrUnBZK3JOU0dxSjREUnN0WkQ0QT09" target="_blank">Calc BC</a></td>
             </tr>
             <tr>
                 <th>4</th>
@@ -24,7 +24,7 @@ def get_schedule(letter, periods):
             <tr>
                 <th>5</th>
                 <td>1:30 PM - 2:25 PM</td>
-                <td>AP Spanish</td>
+                <td><a href="https://us02web.zoom.us/j/86833907107?pwd=aC9SSnQvOGljVDRjQ052UzZ5TGlWZz09" target="_blank">AP Spanish</a></td>
             </tr>
         '''
     elif letter == 'A' and periods == '6-10':
@@ -32,27 +32,27 @@ def get_schedule(letter, periods):
             <tr>
                 <th>6</th>
                 <td>9:10 AM - 10:05 AM</td>
-                <td>Physics Lab/Free</td>
+                <td><a href="https://us02web.zoom.us/j/81053388988?pwd=aGFrRnNuTS9lREVqMmtOekxOT2lTZz09" target="_blank">Physics Lab/Free</a></td>
             </tr>
             <tr>
                 <th>7</th>
                 <td>10:15 AM - 11:10 AM</td>
-                <td>Physics</td>
+                <td><a href="https://us02web.zoom.us/j/81053388988?pwd=aGFrRnNuTS9lREVqMmtOekxOT2lTZz09" target="_blank">Physics</a></td>
             </tr>
             <tr>
                 <th>8</th>
                 <td>11:20 AM - 12:15 PM</td>
-                <td>AP English</td>
+                <td><a href="https://us02web.zoom.us/j/89706035693?pwd=NkRrOXNJSytjR1hGelAyMGVpTWhsQT09" target="_blank">AP English</a></td>
             </tr>
             <tr>
                 <th>9</th>
                 <td>12:25 PM - 1:20 PM</td>
-                <td>Health Ed</td>
+                <td><a href="https://us02web.zoom.us/j/86086092283?pwd=dnpacVBlTmltTzYrWkRMdFNtM0paUT09" target="_blank">Health Ed</a></td>
             </tr>
             <tr>
                 <th>10</th>
                 <td>1:30 PM - 2:25 PM</td>
-                <td>AP Comp Sci</td>
+                <td><a href="https://us02web.zoom.us/j/84387765604?pwd=L2NRR2dITjdmTFpQRC9DTk10WnREZz09" target="_blank">AP Comp Sci</a></td>
             </tr>
         '''
     elif letter == 'B' and periods == '6-10':
@@ -60,27 +60,27 @@ def get_schedule(letter, periods):
             <tr>
                 <th>6</th>
                 <td>9:10 AM - 10:05 AM</td>
-                <td>Phys Ed</td>
+                <td><a href="https://meet.google.com/lookup/djrrqrpag6?authuser=0&hs=179" target="_blank">Phys Ed</a></td>
             </tr>
             <tr>
                 <th>7</th>
                 <td>10:15 AM - 11:10 AM</td>
-                <td>Physics</td>
+                <td><a href="https://us02web.zoom.us/j/81053388988?pwd=aGFrRnNuTS9lREVqMmtOekxOT2lTZz09" target="_blank">Physics</a></td>
             </tr>
             <tr>
                 <th>8</th>
                 <td>11:20 AM - 12:15 PM</td>
-                <td>AP English</td>
+                <td><a href="https://us02web.zoom.us/j/89706035693?pwd=NkRrOXNJSytjR1hGelAyMGVpTWhsQT09" target="_blank">AP English</a></td>
             </tr>
             <tr>
                 <th>9</th>
                 <td>12:25 PM - 1:20 PM</td>
-                <td>Health Ed</td>
+                <td><a href="https://us02web.zoom.us/j/86086092283?pwd=dnpacVBlTmltTzYrWkRMdFNtM0paUT09" target="_blank">Health Ed</a></td>
             </tr>
             <tr>
                 <th>10</th>
                 <td>1:30 PM - 2:25 PM</td>
-                <td>AP Comp Sci</td>
+                <td><a href="https://us02web.zoom.us/j/84387765604?pwd=L2NRR2dITjdmTFpQRC9DTk10WnREZz09" target="_blank">AP Comp Sci</a></td>
             </tr>
         '''
     else:
